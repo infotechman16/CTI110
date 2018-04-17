8553a696d9594361e1c0c0396298c885ac69c52f
infotechman16 <38479101+infotechman16@users.noreply.github.com>
2018-04-17 19:15:51 -0400

    Add files via upload

---
+++ P2T1_Sales Prediction_OquendoRichard2py.py
@@ -0,0 +1,15 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/richa/Desktop/P2T1 Sales Prediction.py ==========
+Enter the projected sales: 1000
+The profit is $ 230.0
+>>> 
+========== RESTART: C:/Users/richa/Desktop/P2T1 Sales Prediction.py ==========
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> 
+========== RESTART: C:/Users/richa/Desktop/P2T1 Sales Prediction.py ==========
+Enter the projected sales: 10000
+The profit is $ 2,300.00
+>>> 
